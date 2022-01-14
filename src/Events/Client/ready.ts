af9d356d1e5cfe8c04f9fecc20b92465c7296c20
Guilherme Nono <Guilherme.nono@hotmail.com>
2022-01-14 18:49:39 -0300

    fix

--- src/Events/Client/ready.ts
+++ src/Events/Client/ready.ts
@@ -2,15 +2,16 @@ import { Event } from "@Interface";
 import { cyan } from "colors";
 import { Contador, MuteRefil, InitDB } from "@Cycle";
 import ExtendedClient from "@Client";
+
 export const event: Event = {
   name: "ready",
   run: async (client: ExtendedClient) => {
     try {
       console.log(cyan(`✨${client.user.tag}'s Online!✨`));
 
-      await InitDB(client)
-      await MuteRefil(client);
-      await Contador(client);
+      // await InitDB(client)
+      // await MuteRefil(client);
+      // await Contador(client);
     } catch (error) {
       console.log(error);
     }
