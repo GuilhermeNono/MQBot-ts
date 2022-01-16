013eddf6e8481b963ace1ef32e3ce16442532aa0
Guilherme Nono <Guilherme.nono@hotmail.com>
2022-01-16 01:39:17 -0300

    teste

--- src/Events/Client/ready.ts
+++ src/Events/Client/ready.ts
@@ -1,15 +1,15 @@
 import { Event } from "../../interfaces/index.js";
 import { cyan } from "colors";
-// import { Contador, MuteRefil, InitDB } from "../Cycle/index.js";
+import { Contador, MuteRefil, InitDB } from "../Cycle/index.js";
 import ExtendedClient from "../../Client/index.js";
 export const event: Event = {
   name: "ready",
   run: async (client: ExtendedClient) => {
     try {
-      // await InitDB(client)
-      console.log(cyan(`✨${client.user.tag}'s Online!✨`));
-      // await MuteRefil(client);
-      // await Contador(client);
+      await InitDB(client)
+      console.log(`✨${client.user.tag}'s Online!✨`);
+      await MuteRefil(client);
+      await Contador(client);
     } catch (error) {
       console.log(error);
     }
