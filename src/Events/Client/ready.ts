6a3e16a5151890177a89c8a9e43935adc98736d8
Guilherme Nono <Guilherme.nono@hotmail.com>
2022-01-17 17:50:28 -0300

    fix

--- src/Events/Client/ready.ts
+++ src/Events/Client/ready.ts
@@ -6,8 +6,8 @@ export const event: Event = {
   name: "ready",
   run: async (client: ExtendedClient) => {
       console.log(`✨${client.user.tag}'s Online!✨`);
-      // await InitDB(client);
-      // await MuteRefil(client);
-      // await Contador(client);
+      await InitDB(client);
+      await MuteRefil(client);
+      await Contador(client);
   },
 };
